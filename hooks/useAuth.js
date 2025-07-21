a93026302abbd497ca156ccd9240b9edbc5cef78
Ryan Hassan <rhassan19996@gmail.com>
2025-07-20 23:00:52 -0400

    create useauth hook and gitignore for google auth

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider value={{ user: 'Don' }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ user: 'null' }}>{children}</AuthContext.Provider>;
 };
 
 export default function useAuth() {
